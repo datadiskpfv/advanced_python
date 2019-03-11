f604bb86224353692aa1921f0d8d82c4c57566e7
pvalle <paul.valle@orbian.com>
2019-03-11 08:53:58 +0000

    looking at comprehensions

--- list_comprehensions/nested_challenge.py
+++ list_comprehensions/nested_challenge.py
@@ -3,7 +3,13 @@
 #
 # The challenge is to use a nested comprehension, to produce the same values.
 # You can iterate over the list, to produce the same output as the for loop, or just print out the list.
- 
+
 for i in range(1, 11):
+    print()
     for j in range(1, 11):
-        print(i, i * j)
+        print(i, i * j, end=", ")
+
+print()
+# same as above
+for times_tables in [[(i, i * j) for j in range(1, 11)] for i in range(1, 11)]:
+    print(times_tables)
